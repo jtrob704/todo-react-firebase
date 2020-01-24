ff33557a86b8a5ff3296595e028f4e41997334df
jtrob704 <jaittarius@hotmail.com>
2020-01-23 19:16:18 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './Todos/Todos'
+// eslint-disable-next-line
+import { firebaseConfig } from './Config/config'
+// eslint-disable-next-line
+import firebase from 'firebase/app'
+import 'firebase/database'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">        
+          <h1>ToDo</h1>
+          <Todos />
+        </header>
+      </div>
+    );
+    }
+  }
 
 export default App;
---
+++ src/Todos/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Todos extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Todos list</h2>
+            </div>
+        )
+    }
+}
+
+export default Todos
